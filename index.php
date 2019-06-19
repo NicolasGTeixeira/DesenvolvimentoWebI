2b0b48dff3dbb16f7549a63c574ce153610db07b
nicolas <nicolas@cpqd.com.br>
2019-06-19 16:53:10 -0300

    Correcao da mensagem de usuario invalido na tela: index.php

--- index.php
+++ index.php
@@ -23,8 +23,10 @@
                       <div class="notification is-danger">
                       <p>ERRO: Usuário ou senha inválidos.</p>
                     </div>
+                    
                     <?
-                    endif
+                    endif;
+                    unset($_SESSION['naoAutenticado']);
                     ?>
                     <div class="box">
                         <form action="login.php" method="POST">
@@ -33,7 +35,6 @@
                                     <input name="usuario" name="text" class="input is-large" placeholder="Seu usuário" autofocus="">
                                 </div>
                             </div>
-
                             <div class="field">
                                 <div class="control">
                                     <input name="senha" class="input is-large" type="password" placeholder="Sua senha">
